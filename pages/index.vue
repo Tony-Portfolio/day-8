9ecb72cdee791647160bc03a75dbf8839d634eb6
Tony <84824688+Tony-Portfolio@users.noreply.github.com>
2023-05-22 04:01:50 +0000

    Update commit

--- pages/index.vue
+++ pages/index.vue
@@ -40,7 +40,7 @@
                                 untuk membuat struktur dan konten halaman web.</p>
                         </div>
                         <div class="flex justify-start">
-                            <NuxtLink href="/class/html-html.jpg"
+                            <NuxtLink href="/class/1"
                                 class="bg-[#6d4dff] text-white p-2 rounded px-4 block w-fit">Mulai Kelas</NuxtLink>
                         </div>
                     </div>
@@ -61,7 +61,7 @@
                                 mengatur tampilan dan tata letak elemen-elemen dalam halaman web.</p>
                         </div>
                         <div class="flex justify-start">
-                            <NuxtLink href="/class/css-css.jpeg"
+                            <NuxtLink href="/class/2"
                                 class="bg-[#6d4dff] text-white p-2 rounded px-4 block w-fit">Mulai Kelas</NuxtLink>
                         </div>
                     </div>
@@ -82,7 +82,7 @@
                                 web interaktif dan dinamis.</p>
                         </div>
                         <div class="flex justify-start">
-                            <NuxtLink href="/class/javascript-javascript.jpg"
+                            <NuxtLink href="/class/3"
                                 class="bg-[#6d4dff] text-white p-2 rounded px-4 block w-fit">Mulai Kelas</NuxtLink>
                         </div>
                     </div>
@@ -102,7 +102,7 @@
                                 pengembangan web.</p>
                         </div>
                         <div class="flex justify-start">
-                            <NuxtLink href="/class/php-php.jpg"
+                            <NuxtLink href="/class/4"
                                 class="bg-[#6d4dff] text-white p-2 rounded px-4 block w-fit">Mulai Kelas</NuxtLink>
                         </div>
                     </div>
